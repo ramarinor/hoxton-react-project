11e83ac42a17e292b077299924482a14974c0569
Rinor Rama <rinor.rama95@gmail.com>
2022-01-31 19:54:54 +0100

    added header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,47 @@
+import { Link } from "react-router-dom";
+import "./Header.css";
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <nav className="nav-bar">
+        <ul className="nav-bar-list">
+          <li className="nav-bar-item">
+            <Link to="/paints" className="nav-bar-link">
+              PAINTS
+            </Link>
+          </li>
+          <li className="nav-bar-item">
+            <Link to="/samples" className="nav-bar-link">
+              SAMPLES
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Link to="/" className="logo-link">
+        <h1 className="logo">PALETTE</h1>
+      </Link>
+
+      <nav className="nav-bar">
+        <ul className="nav-bar-list">
+          <li className="nav-bar-item">
+            <Link to="/calculator" className="nav-bar-link">
+              CALCULATOR
+            </Link>
+          </li>
+          <li className="nav-bar-item">
+            <Link to="/Login" className="nav-bar-link">
+              LOG IN
+            </Link>
+          </li>
+          <li className="nav-bar-item">
+            <Link to="/cart" className="nav-bar-link">
+              CART (0)
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
